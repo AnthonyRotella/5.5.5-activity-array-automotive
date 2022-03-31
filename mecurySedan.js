ee7f65312e41a5e121d962391aec6259aeff8760
Anthony Rotella <mr4406@yahoo.com>
2022-03-30 22:01:53 -0500

    Mercury

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,33 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+        } else {
+            return this.started = false;
+            console.log("no fuel");
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " Ill have to come back, full up.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " too many peeps");
+        }
+    }
--- vehicle.js
+++ vehicle.js
@@ -10,14 +10,38 @@ class Vehicle {
         this.started = false;
         this.numberOfWheels = 0;
     }
+}
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
 
     start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
         } else {
             return this.started = false;
-            console.log("engine cannot start...");
+            console.log("no fuel");
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " Ill have to come back, full up.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " too many peeps");
         }
     }
     accelerate() {
@@ -86,6 +110,6 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+//module.exports = {
+    //Vehicle
+//}
